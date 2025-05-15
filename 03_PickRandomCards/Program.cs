39cf737fba075e64be40c875913ff7ac8f7ff851
Henrique <henriquebuzzi1997@gmail.com>
2025-05-15 17:41:48 -0300

    Projeto 03 Pick Random Cards

---
+++ 03_PickRandomCards/Program.cs
@@ -0,0 +1,19 @@
+﻿using _03_PickRandomCards;
+
+Console.WriteLine("Enter the number of cards to pick: ");
+
+string line = Console.ReadLine();
+
+if(int.TryParse(line, out int numberOfCards))
+{
+    string[] deck = CardPicker.PickSomeCards(numberOfCards);
+    foreach(string cards in deck)
+    {
+        Console.WriteLine(cards);
+    }
+}
+else
+{
+    Console.WriteLine("Just numbers are valid");
+}
+
